70d81a927da8d1c747f4840ac7457ee58a7f79e5
JMSajib <jm.sajib012@gmail.com>
2018-07-04 10:54:19 +0600

    Added Sidedrawer and Responsive Design

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,23 +3,24 @@ import React from 'react';
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const burger = (props) =>{
-    let transformIngredients = Object.keys(props.ingredients).map(key =>{
-        return [...Array(props.ingredients[key])].map((_, i)=>{
-            return <BurgerIngredient key={key + i} type={key}/>;
-        });
-      })
-      .reduce((arr,el) =>{
-        return arr.concat(el)
-      },[]);
-      if(transformIngredients.length === 0){
-          transformIngredients = <p>Please add ingredients!</p>;
-      }
-    return(
+const burger = ( props ) => {
+    let transformedIngredients = Object.keys( props.ingredients )
+        .map( igKey => {
+            return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
+            } );
+        } )
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>;
+    }
+    return (
         <div className={classes.Burger}>
-            <BurgerIngredient type="bread-top"/>
-            {transformIngredients}
-            <BurgerIngredient type="bread-bottom"/>
+            <BurgerIngredient type="bread-top" />
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom" />
         </div>
     );
 };
--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,32 +1,30 @@
 import React from 'react';
+
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) =>{
-    const style = {
-        textTransform:'capitalize'
-    };
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(igKey=>{
+const orderSummary = ( props ) => {
+    const ingredientSummary = Object.keys( props.ingredients )
+        .map( igKey => {
             return (
                 <li key={igKey}>
-                    <span style={style}>{igKey}</span>: {props.ingredients[igKey]}
-                </li>
-            );
-        });
-    return(
+                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
+                </li> );
+        } );
+
+    return (
         <Aux>
             <h3>Your Order</h3>
-            <p>A Delicious Burger with following Ingredients:</p>
+            <p>A delicious burger with the following ingredients:</p>
             <ul>
                 {ingredientSummary}
             </ul>
             <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
+            <p>Continue to Checkout?</p>
+            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
             <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
         </Aux>
-    )
+    );
 };
 
 export default orderSummary;
